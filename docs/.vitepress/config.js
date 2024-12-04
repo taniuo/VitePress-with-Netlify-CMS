d018227b403885a2919fa9b1a9bc9597d038a574
taniuo <temp@aniuo.com>
2024-12-04 13:05:15 +0800

    Create config.js

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  title: 'VitePress template',
+  description: 'A JAMstack website template with the VitePress and Netlify CMS config.',
+  themeConfig: {
+    repo: 'taniuo/VitePress-with-Netlify-CMS',
+    docsDir: 'docs',
+    editLinks: true,
+    editLinkText: 'Edit this page on GitHub',
+    lastUpdated: 'Last Updated',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'Posts', link: '/post/' }
+    ],
+    sidebar: {
+      '/post/': [
+        {
+          text: 'Posts',
+          // 自动生成 `docs/post` 目录下的文章目录
+          children: ['/post/']
+        }
+      ],
+      // 其他路径可以保持 'auto' 或手动配置
+      '/': 'auto'
+    }
+  }
+}
