dfdcd58786ed3dbb2c7eb8056535386b7af92689
taniuo <temp@aniuo.com>
2024-12-04 23:47:20 +0800

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -15,3 +15,15 @@ module.exports = {
 		],
 	}
 }
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Pages from 'vite-plugin-pages';
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    Pages({
+      // 可以在这里自定义选项
+    }),
+  ],
+});
