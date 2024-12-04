d47224748a38569aec4424b7b50dd05c103e8322
taniuo <temp@aniuo.com>
2024-12-04 12:01:35 +0800

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -12,5 +12,16 @@ module.exports = {
 			{ text: 'Guide', link: '/guide/' },
 			{ text: 'GitHub', link: 'https://github.com/tsutoringo/VitePress-with-Netlify-CMS' }
 		],
+    		sidebar: {
+      		'/post/': [
+        		{
+          		text: 'Posts',
+          		// 自动生成 `docs/post` 目录下的文章目录
+          		children: ['/post/']
+        		}
+      		],
+      		// 其他路径可以保持 'auto' 或手动配置
+      		'/': 'auto'
+    		}
 	}
 }
