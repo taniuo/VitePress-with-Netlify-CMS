62220e1324f644b4f519e21155f5d2b06d9911b4
taniuo <temp@aniuo.com>
2024-12-04 11:14:55 +0800

    Create _dir_.vue

---
+++ docs/_dir_.vue
@@ -0,0 +1,22 @@
+<!-- docs/blog/_dir_.vue -->
+<template>
+  <div>
+    <h1>博客文章</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.slug">
+        <router-link :to="post.path">{{ post.title }}</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { useData } from 'vitepress'
+import { computed } from 'vue'
+
+const { theme } = useData()
+
+const posts = computed(() => {
+  return theme.value.posts || []
+})
+</script>
